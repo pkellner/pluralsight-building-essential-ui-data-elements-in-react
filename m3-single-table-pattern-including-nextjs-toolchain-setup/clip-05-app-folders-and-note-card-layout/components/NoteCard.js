2068793b9185bf553a2dc57ee4226537889cb3da
pkellner <peter@peterkellner.net>
2021-09-16 15:00:20 -0700

    working

--- m3-single-table-pattern-including-nextjs-toolchain-setup/clip-05-app-folders-and-note-card-layout/components/NoteCard.js
+++ m3-single-table-pattern-including-nextjs-toolchain-setup/clip-05-app-folders-and-note-card-layout/components/NoteCard.js
@@ -1,22 +1,29 @@
 function NoteCard({ note }) {
   return (
-    <div className="col-md-4 single-note-item all-category">
+    <div className="col-md-4 signle-note-item all-category">
       <div className="card card-body">
-        <span className="side-stick"></span>
-      </div>
-      <p className="note-date font-12 text-muted">
-        {new Date(note.createDate).toLocaleTimeString("en", {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-          hour: "2-digit",
-          minute: "2-digit",
-        })}
-      </p>
-      <div className="note-content">
-        <p className="note-inner-content text-muted">
-          {note.description}
+        <div>
+          <span className="side-stick"></span>
+          <h5 className="note-title text-truncate w-75 mb-0">
+            {note.title}
+          </h5>
+        </div>
+
+        <p className="note-date font-12 text-muted">
+          {new Date(note.createDate).toLocaleTimeString("en", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "2-digit",
+            minute: "2-digit"
+          })}
         </p>
+
+        <div className="note-content">
+          <p className="note-inner-content text-muted">
+            {note.description}
+          </p>
+        </div>
       </div>
     </div>
   );
