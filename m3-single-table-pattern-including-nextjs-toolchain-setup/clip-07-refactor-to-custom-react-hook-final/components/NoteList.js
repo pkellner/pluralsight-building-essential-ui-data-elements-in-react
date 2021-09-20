a6c88ca118c612a668e4932c15a17499429a0011
pkellner <peter@peterkellner.net>
2021-09-20 16:41:24 -0700

    wip

--- m3-single-table-pattern-including-nextjs-toolchain-setup/clip-07-refactor-to-custom-react-hook-final/components/NoteList.js
+++ m3-single-table-pattern-including-nextjs-toolchain-setup/clip-07-refactor-to-custom-react-hook-final/components/NoteList.js
@@ -2,29 +2,8 @@ import NoteCard from "./NoteCard.js";
 import notes from "../data/notes.json";
 import { useEffect, useState } from 'react';
 
-function NoteList() {
+function NoteList( {notesData}) {
 
-  const [notesData, setNotesData] = useState();
-  const [notesDataError, setNotesDataError] = useState();
-
-  useEffect(() => {
-    async function getData() {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      try {
-        setNotesData(notes);
-      } catch (e) {
-        setNotesDataError(e);
-      }
-    }
-    getData();
-  });
-
-  if (notesDataError) {
-    return <div>error: {notesDataError}</div>;
-  }
-  if (!notesData) {
-    return <div>...loading</div>;
-  }
   function sortByDate(a, b) {
     const dateA = a.createDate;
     const dateB = b.createDate;
