43e0712474b98c26a180225cf2151119ec9ee552
pkellner <peter@peterkellner.net>
2021-09-20 17:46:43 -0700

    wip

---
+++ m3-single-table-pattern-including-nextjs-toolchain-setup/clip-07-refactor-to-custom-react-hook/components/App.js
@@ -0,0 +1,22 @@
+import NoteList from './NoteList.js';
+import useNotes from '../hooks/useNotes';
+
+function App() {
+
+  const { notesData, notesDataError } = useNotes();
+
+  if (notesDataError) {
+    return <div className="container">error: {notesDataError}</div>;
+  }
+  if (!notesData) {
+    return <div className="container">...loading</div>;
+  }
+
+  return (
+    <div className="container">
+      <NoteList notesData={notesData} />
+    </div>
+  );
+}
+
+export default App;
