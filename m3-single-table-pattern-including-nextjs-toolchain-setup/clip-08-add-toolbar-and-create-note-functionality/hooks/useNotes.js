f87c2632adb800db9e2fc702a8760205cd140829
pkellner <peter@peterkellner.net>
2021-09-20 18:09:58 -0700

    wip

---
+++ m3-single-table-pattern-including-nextjs-toolchain-setup/clip-08-add-toolbar-and-create-note-functionality/hooks/useNotes.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import notes from '../data/notes.json';
+import { v4 as uuidv4 } from 'uuid';
+
+
+function useNotes() {
+
+  const [notesData, setNotesData] = useState();
+  const [notesDataError, setNotesDataError] = useState();
+
+  useEffect(() => {
+    async function getData() {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      try {
+        setNotesData(notes);
+      } catch (e) {
+        setNotesDataError(e);
+      }
+    }
+    getData();
+  },[]);
+
+  function createNote(title, description) {
+    const newNote = {
+      id: uuidv4(),
+      title, description, createDate: new Date().toISOString(),
+    };
+    setNotesData((oldNotes) => {
+      return [...oldNotes, newNote]; // Order does not matter as sort happens later
+    });
+  }
+
+  return { notesData, notesDataError, createNote };
+
+}
+
+export default useNotes;
