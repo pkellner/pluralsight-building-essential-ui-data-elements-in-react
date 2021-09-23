3949a3ed2e6ea6829a1b88fb0696bd08cb603ee6
pkellner <peter@peterkellner.net>
2021-09-23 16:14:00 -0700

    wip

---
+++ m4-adding-insert-update-delete-to-notes-app/clip-03-add-update-and-delete-local-component-state/components/NoteList.js
@@ -0,0 +1,20 @@
+import NoteCard from "./NoteCard.js";
+
+function NoteList({ notesData}) {
+
+  function sortByDate(a, b) {
+    const dateA = a.createDate;
+    const dateB = b.createDate;
+    return dateA > dateB ? -1 : dateA < dateB ? 1 : 0;
+  }
+  
+  return (
+    <div className="row tab-content bg-transparent note-has-grid">
+      {notesData.sort(sortByDate).map((note) => {
+        return <NoteCard note={note} key={note.id} />;
+      })}
+    </div>
+  );
+}
+
+export default NoteList;
---
+++ m4-adding-insert-update-delete-to-notes-app/clip-03-add-update-and-delete-local-component-state/components/menu.js
@@ -0,0 +1,15 @@
+function Menu({ createNoteFn}) {
+  return (
+    <ul className="nav nav-pills p-3 bg-white mb-3 founded-pill align-items-center">
+      <li className="nav-item ml-auto">
+        <a href="#" onClick={createNoteFn}
+          className="nav-link btn-primary rounded-pill d-flex align-items-center px-3">
+          <i className="icon-note m-1"></i>
+          <span className="d-none d-md-block font-14">Add Notes</span>
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default Menu;
