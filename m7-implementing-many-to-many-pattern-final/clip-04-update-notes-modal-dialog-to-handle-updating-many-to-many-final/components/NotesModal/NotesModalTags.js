6ec9689f5595753f145ad422e28592917dd70bbc
pkellner <peter@peterkellner.net>
2021-10-16 18:58:44 -0700

    wp

--- m7-implementing-many-to-many-pattern-final/clip-04-update-notes-modal-dialog-to-handle-updating-many-to-many-final/components/NotesModal/NotesModalTags.js
+++ m7-implementing-many-to-many-pattern-final/clip-04-update-notes-modal-dialog-to-handle-updating-many-to-many-final/components/NotesModal/NotesModalTags.js
@@ -9,71 +9,76 @@ function NotesModalTags() {
     setTagNamesNewValue,
   } = useContext(NotesModalContext);
   const { tagsData } = useContext(NotesContext);
-  //if (!tagsData) return null;
 
   return (
-    <>
-      <div>{JSON.stringify(modalNoteTagIds)}</div>
-      <div className="notes-box  margin-left-right-15">
-        <div className="container">
-          <h6>Tags</h6>
-          <div className="row">
-            {tagsData
-              ? [...tagsData]
-                  .sort(function (a, b) {
-                    const textA = a.tagName.toUpperCase();
-                    const textB = b.tagName.toUpperCase();
-                    return textA < textB ? -1 : textA > textB ? 1 : 0;
-                  })
-                  .map((rec) => {
-                    return (
-                      <div className="col-2" key={rec.id}>
-                        <input
-                          className="form-check-input"
-                          type="checkbox"
-                          id={`formchecklabelid-${rec.id}`}
-                          onChange={(e) => {
-                            if (e.target.checked) {
-                              setModalNoteTagIds([...modalNoteTagIds, rec.id]);
-                            } else {
-                              setModalNoteTagIds(
-                                modalNoteTagIds.filter((r) => r != rec.id)
-                              );
-                            }
-                          }}
-                          checked={
-                            modalNoteTagIds.includes(rec.id) ? true : false
-                          }
-                        />
-                        <label
-                          className="form-check-label"
-                          htmlFor={`formchecklabelid-${rec.id}`}
-                        >
-                          {rec.tagName}
-                        </label>
-                      </div>
-                    );
-                  })
-              : null}
-          </div>
-          <div className="row">
-            <input
-              value={tagNamesNewValue}
-              onChange={(event) => {
-                setTagNamesNewValue(event.target.value);
-              }}
-              type="text"
-              className="form-control font-size-smaller margin-top-10"
-              placeholder="New Tags (csv)"
-            />
+    <div className="modal-body">
+      <div className="notes-box">
+        <div className="notes-content">
+          <div className="d-flex align-items-center">
+            <div className="mr-2 container">
+              <div className="row">
+                {tagsData
+                  ? [...tagsData]
+                      .sort(function (a, b) {
+                        const textA = a.tagName.toUpperCase();
+                        const textB = b.tagName.toUpperCase();
+                        return textA < textB ? -1 : textA > textB ? 1 : 0;
+                      })
+                      .map((rec) => {
+                        return (
+                          <div
+                            className="col-2 margin-left-right-15"
+                            key={rec.id}
+                          >
+                            <input
+                              className="form-check-input"
+                              type="checkbox"
+                              id={`formchecklabelid-${rec.id}`}
+                              onChange={(e) => {
+                                if (e.target.checked) {
+                                  setModalNoteTagIds([
+                                    ...modalNoteTagIds,
+                                    rec.id,
+                                  ]);
+                                } else {
+                                  setModalNoteTagIds(
+                                    modalNoteTagIds.filter((r) => r != rec.id)
+                                  );
+                                }
+                              }}
+                              checked={
+                                modalNoteTagIds.includes(rec.id) ? true : false
+                              }
+                            />
+                            <label
+                              className="form-check-label"
+                              htmlFor={`formchecklabelid-${rec.id}`}
+                            >
+                              {rec.tagName}
+                            </label>
+                          </div>
+                        );
+                      })
+                  : null}
+              </div>
+            </div>
+            <div className="mr-2 container">
+              <div className="row">
+                <input
+                  value={tagNamesNewValue}
+                  onChange={(event) => {
+                    setTagNamesNewValue(event.target.value);
+                  }}
+                  type="text"
+                  className="form-control font-size-smaller margin-top-10"
+                  placeholder="New Tags (csv)"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      {/*<hr />*/}
-      {/*<div className="modal-body">*/}
-      {/*  modalNoteTagIds: {JSON.stringify(modalNoteTagIds)}*/}
-      {/*</div>*/}
-    </>
+    </div>
   );
 }
 export default NotesModalTags;
