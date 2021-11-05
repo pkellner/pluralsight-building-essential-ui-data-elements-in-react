1183997b8ced4507205cb197f39ff1dfc3755cb6
pkellner <peter@peterkellner.net>
2021-11-05 13:29:54 -0700

    wip

--- m5-implementing-1-to-t-data-pattern/clip-04-state-management-for-1-to-1/hooks/useNotes.js
+++ m5-implementing-1-to-t-data-pattern/clip-04-state-management-for-1-to-1/hooks/useNotes.js
@@ -36,25 +36,22 @@ function useNotes() {
       description,
     };
     updateNotesData(id, updateObject);
-    if (pinned || important) {
-      const noteAttributes = noteAttributesData.find(
-        (rec) => rec.noteId === id
-      );
-      if (noteAttributes) {
-        updateNoteAttributesData(noteAttributes.id, {
-          pinned: pinned ? 1 : 0,
-          important: important ? 1 : 0,
-          updateDate: new Date().toISOString(),
-        });
-      } else {
-        createNoteAttributesData(noteAttributes.id, {
-          id: uuidv4(),
-          noteId: id,
-          pinned: pinned ? 1 : 0,
-          important: important ? 1 : 0,
-          updateDate: new Date().toISOString(),
-        });
-      }
+
+    const noteAttributes = noteAttributesData.find((rec) => rec.noteId === id);
+    if (noteAttributes) {
+      updateNoteAttributesData(noteAttributes.id, {
+        pinned: pinned ? 1 : 0,
+        important: important ? 1 : 0,
+        updateDate: new Date().toISOString(),
+      });
+    } else {
+      createNoteAttributesData(noteAttributes.id, {
+        id: uuidv4(),
+        noteId: id,
+        pinned: pinned ? 1 : 0,
+        important: important ? 1 : 0,
+        updateDate: new Date().toISOString(),
+      });
     }
   }
 
