bb6fdf525d3562e1c7740bd2545ac3cf5bea77fa
pkellner <peter@peterkellner.net>
2021-11-19 18:14:04 -0800

    wip

---
+++ m8-persisting-to-sql/clip-08-code-remaning-4-entities-with-rest-and-sqlite/pages/api/noteChangeLogs/[id].js
@@ -0,0 +1,6 @@
+import { processGetOnePutAndDelete } from "../../../lib/restUtils";
+import prisma from "../../../lib/prisma";
+
+export default async function handle(req, res) {
+  await processGetOnePutAndDelete(prisma.noteChangeLog, req, res);
+}
